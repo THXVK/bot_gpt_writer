ea3511362d046009c06dcb29756976279b7a7116
Артём Сергеевич <artemsavelev356@gmail.com>
2024-03-24 19:45:08 +0300

    прикрутил логирование

--- code/config.py
+++ code/config.py
@@ -6,14 +6,13 @@ from os import getenv
 
 GPT_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
 MAX_USERS = 3
-MAX_TOKENS = 75
 MAX_SESSIONS = 3
-MAX_TOKENS_PER_SESSION = 800
-MAX_MODEL_TOKENS = 75
+MAX_TOKENS_PER_SESSION = 15
+MAX_MODEL_TOKENS = 5
 ADMINS_ID = [1999763430]
 LOGS_PATH = 'other/logConfig.log'
-METADATA_URL = "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token"
-IM_TOKEN_PATH = 'other/token.json'
+IAM_TOKEN_PATH = 'other/token.json'
+IAM_TOKEN_ENDPOINT = "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token"
 load_dotenv()
 
 IAM_TOKEN = getenv('IAM_TOKEN')
--- code/gpt.py
+++ code/gpt.py
@@ -1,41 +1,59 @@
 
 import requests
 
+from log import logger
 from data import get_user_data
-from config import GPT_URL, LOGS_PATH, MAX_MODEL_TOKENS, FOLDER_ID, METADATA_URL, IAM_TOKEN, IM_TOKEN_PATH
+from config import GPT_URL, LOGS_PATH, MAX_MODEL_TOKENS, FOLDER_ID, IAM_TOKEN, IAM_TOKEN_PATH, IAM_TOKEN_ENDPOINT
 import time
 import json
 
 
-
-
-def create_new_token():
+def create_new_iam_token():
+    """
+    Получает новый IAM-TOKEN и дату истечения его срока годности и записывает полученные данные в json
+    """
+    headers = {"Metadata-Flavor": "Google"}
     try:
-        headers = {"Metadata-Flavor": "Google"}
-        response = requests.get(METADATA_URL, headers=headers)
+        response = requests.get(IAM_TOKEN_ENDPOINT, headers=headers)
+
+    except Exception as e:
+        error_msg = f"Ошибка: {e}"
+        logger.error(error_msg)
+
+    else:
         if response.status_code == 200:
-            token_data = response.json()
-            token_data['expires_at'] = time.time() + token_data['expires_in']
-            with open(IM_TOKEN_PATH, 'w') as token_file:
-                json.dump(token_data)
-        return response.json()
-    except Exception:
-        pass
+            token_data = {
+                "access_token": response.json().get("access_token"),
+                "expires_at": response.json().get("expires_in") + time.time()
+            }
+            with open(IAM_TOKEN_PATH, "w") as token_file:
+                json.dump(token_data, token_file)
+        else:
+            error_msg = f"Ошибка: {response.status_code}"
+            logger.error(error_msg)
 
 
-def get_aim_token():
+def get_iam_token() -> str:
+    """
+    Получает действующий IAM-TOKEN и возвращает его
+    """
     try:
-        with open(IM_TOKEN_PATH, 'r') as file:
-            data = json.load(file)
-            expiration = data['expires_at']
+        with open(IAM_TOKEN_PATH, "r") as token_file:
+            token_data = json.load(token_file)
 
-        if expiration < time.time():
-            return
-    except Exception:
-        pass
+        expires_at = token_data.get("expires_at")
+
+        if expires_at <= time.time():
+            create_new_iam_token()
+
+    except FileNotFoundError:
+        create_new_iam_token()
+    with open(IAM_TOKEN_PATH, "r") as token_file:
+        token_data = json.load(token_file)
+    return token_data.get("access_token")
 
 
-def gen_promt(user_id):
+def gen_promt(user_id) -> str:
     data = get_user_data(user_id)
     params = {
         'character': data[4],
@@ -52,6 +70,7 @@ def gen_promt(user_id):
 
 def gpt_start(user_id):
     promt = gen_promt(user_id)
+    iam_token = IAM_TOKEN
     data = {
         "modelUri": f"gpt://{FOLDER_ID}/yandexgpt-lite",
         "completionOptions": {
@@ -69,7 +88,7 @@ def gpt_start(user_id):
 
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {IAM_TOKEN}",
+        "Authorization": f"Bearer {iam_token}",
         "x-folder-id": f"{FOLDER_ID}",
     }
 
@@ -78,15 +97,21 @@ def gpt_start(user_id):
         headers=headers,
         json=data
     )
-    result = response.json()['result']['alternatives'][0]['message']['text']
-    tokens = response.json()['result']['usage']['totalTokens']
-    print(tokens)
-    return {'result': result, 'tokens': tokens}
+    if response.status_code == 200:
+        result = response.json()['result']['alternatives'][0]['message']['text']
+        tokens = response.json()['result']['usage']['completionTokens']
+
+        return {'result': result, 'tokens': int(tokens)}
+    else:
+        error_msg = f"Ошибка: {response.status_code}"
+        logger.error(error_msg)
+        return {'result': 'что то пошло не так', 'tokens': 0}
 
 
-def gpt_end(text, user_id):
+def gpt_ask(text, user_id):
     promt = gen_promt(user_id)
     story = get_user_data(user_id)[8]
+    iam_token = IAM_TOKEN
     data = {
         "modelUri": f"gpt://{FOLDER_ID}/yandexgpt-lite",
         "completionOptions": {
@@ -113,7 +138,7 @@ def gpt_end(text, user_id):
 
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {IAM_TOKEN}",
+        "Authorization": f"Bearer {iam_token}",
         "x-folder-id": f"{FOLDER_ID}",
     }
 
@@ -122,15 +147,21 @@ def gpt_end(text, user_id):
         headers=headers,
         json=data
     )
-    result = response.json()['result']['alternatives'][0]['message']['text']
-    tokens = response.json()['result']['usage']['totalTokens']
+    if response.status_code == 200:
+        result = response.json()['result']['alternatives'][0]['message']['text']
+        tokens = response.json()['result']['usage']['completionTokens']
 
-    return {'result': result, 'tokens': tokens}
+        return {'result': result, 'tokens': int(tokens)}
+    else:
+        error_msg = f"Ошибка: {response.status_code}"
+        logger.error(error_msg)
+        return {'result': 'что то пошло не так', 'tokens': 0}
 
 
-def gpt_ask(text, user_id):
+def gpt_end(user_id) -> str:
     promt = gen_promt(user_id)
     story = get_user_data(user_id)[8]
+    iam_token = IAM_TOKEN
     data = {
         "modelUri": f"gpt://{FOLDER_ID}/yandexgpt-lite",
         "completionOptions": {
@@ -151,7 +182,7 @@ def gpt_ask(text, user_id):
     }
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {IAM_TOKEN}",
+        "Authorization": f"Bearer {iam_token}",
         "x-folder-id": f"{FOLDER_ID}",
     }
 
@@ -160,6 +191,11 @@ def gpt_ask(text, user_id):
         headers=headers,
         json=data
     )
-    result = response.json()['result']['alternatives'][0]['message']['text']
+    if response.status_code == 200:
+        result = response.json()['result']['alternatives'][0]['message']['text']
+        return result
 
-    return result
+    else:
+        error_msg = f"Ошибка: {response.status_code}"
+        logger.error(error_msg)
+        return 'что то пошло не так'
